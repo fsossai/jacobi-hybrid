a6e67db5ed0c71a6d9f4111fce287b26c7fcfe52
Federico Sossai <federico.sossai@gmail.com>
2020-09-11 09:09:41 +0200

    Strong and weak scaling are now separated type of benchmarks

--- jacobi-one-sided/benchmarks/benchmark-general.sh
+++ jacobi-one-sided/benchmarks/benchmark-general.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-if (( $# < 2 )); then
-	echo "Usage: $0 <cluster_name> <mode>"
+if (( $# < 3 )); then
+	echo "Usage: $0 <cluster_name> <mode> <scaling>"
 	echo "mode:	intranode | internode | topology"
 	exit 1
 fi
 
 cluster_name=$1
 mode=$2
+scaling=$3
 echo "Nodes = $SLURM_JOB_NODELIST"
 echo "Maximum number of MPI processes: $SLURM_NTASKS"
 time_start=$(date +%s)
 
 if [[ $mode =~ ^(intranode|internode) ]]; then
-	./benchmark-scaling.sh $cluster_name $SLURM_NTASKS_PER_NODE $SLURM_JOB_NUM_NODES $mode
+	if [[ $scaling =~ ^(strong|weak) ]]; then
+		./benchmark-scaling.sh $cluster_name $SLURM_NTASKS_PER_NODE $SLURM_JOB_NUM_NODES $mode $scaling
+	else
+		echo "ERROR: <scaling> must be 'strong' or 'weak'"
+		exit 1
+	fi
 elif [ "$mode" = "topology" ]; then
 	./benchmark-topology.sh $cluster_name $SLURM_NTASKS
 else
-	echo "ERROR: Unrecognized argument '$mode'"
-	echo "Available arguments: 'intranode', 'internode', 'topology'"
+	echo "ERROR: <mode> must be 'intranode', 'internode' or 'topology'"
 	exit 1
 fi
 
--- jacobi-one-sided/benchmarks/benchmark-scaling.sh
+++ jacobi-one-sided/benchmarks/benchmark-scaling.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if (( $# < 4 )); then
-    echo "Usage: $0 <cluster_name> <ppn> <nnodes> <mode>"
+if (( $# < 5 )); then
+    echo "Usage: $0 <cluster_name> <ppn> <nnodes> <mode> <scaling>"
     echo "mode: intranode | internode"
+    echo "scaling: strong | weak"
     exit 1
 fi
 
@@ -10,29 +11,43 @@ cluster_name=$1
 ppn=$2
 nnodes=$3
 mode=$4
+scaling=$5
+
+if [[ ! $scaling =~ ^(strong|weak) ]]; then
+    echo "ERROR: <scaling> must be 'strong' or 'weak'"
+    exit 1
+fi
+
+# Set here the problem sizes
 
 if [ "$mode" = "intranode" ]; then
-	nnodes=""
-    start_size_2d=20000
-    start_size_3d=700
+    nnodes=""
+    iterations=50
+    if [ "$scaling" = "strong" ]; then
+        start_size_2d=20000
+        start_size_3d=700
+    elif [ "$scaling" = "weak" ]; then
+        start_size_2d=4000
+        start_size_3d=250
+    fi
 elif [ "$mode" = "internode" ]; then
-	nnodes=$SLURM_JOB_NUM_NODES
-    start_size_2d=44000
-    start_size_3d=1200
+    nnodes=$SLURM_JOB_NUM_NODES
+    iterations=50
+    if [ "$scaling" = "strong" ]; then
+        start_size_2d=44000
+        start_size_3d=1200
+    elif [ "$scaling" = "weak" ]; then
+        start_size_2d=44000
+        start_size_3d=1200
+    fi
 else
     echo "ERROR: Wrong argument for <mode> ($mode)"
     echo "mode: intranode | internode"
     exit 1
 fi
 
-echo "Running ${mode}-scaling.sh (2D / strong) on $cluster_name"
-./${mode}-scaling.sh 2 $start_size_2d 50 $ppn $nnodes strong $cluster_name
+echo "Running ${mode}-scaling.sh (2D / $scaling) on $cluster_name"
+./${mode}-scaling.sh 2 $start_size_2d $iterations $ppn $nnodes $scaling $cluster_name
 
-echo "Running ${mode}-scaling.sh (3D / strong) on $cluster_name"
-./${mode}-scaling.sh 3 $start_size_3d 50 $ppn $nnodes strong $cluster_name
-
-echo "Running ${mode}-scaling.sh (2D / weak) on $cluster_name"
-./${mode}-scaling.sh 2 $start_size_2d 50 $ppn $nnodes weak $cluster_name
-
-echo "Running ${mode}-scaling.sh (3D / weak) on $cluster_name"
-./${mode}-scaling.sh 3 $start_size_3d 50 $ppn $nnodes weak $cluster_name
+echo "Running ${mode}-scaling.sh (3D / $scaling) on $cluster_name"
+./${mode}-scaling.sh 3 $start_size_3d $iterations $ppn $nnodes $scaling $cluster_name
